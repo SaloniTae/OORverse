ae3d6157fa938115aaf5290cad60a939d1354eb3
SaloniTae <98252350+SaloniTae@users.noreply.github.com>
2025-10-04 18:28:29 +0530

    Update firebase-nswf.ts

--- src/lib/firebase-nswf.ts
+++ src/lib/firebase-nswf.ts
@@ -4,15 +4,15 @@ import { getDatabase, ref, get, set, remove, update, onValue, off } from "fireba
 
 // Firebase configuration for NSWF
 const firebaseConfig = {
-  apiKey: "AIzaSyA02dPt8yMTSmhzyj9PIrm4UlWr1a1waD4",
-  authDomain: "testing-6de54.firebaseapp.com",
-  databaseURL: "https://testing-6de54-default-rtdb.firebaseio.com",
-  projectId: "testing-6de54",
-  storageBucket: "testing-6de54.firebasestorage.app",
-  messagingSenderId: "159795986690",
-  appId: "1:159795986690:web:2e4de44d725826dc01821b"
+  apiKey: "AIzaSyAYRZD50jryWCXTkoVIGgyBiilYz8VaI0Y",
+  authDomain: "get-account-18-43109.firebaseapp.com",
+  databaseURL: "https://get-account-18-43109-default-rtdb.firebaseio.com",
+  projectId: "get-account-18-43109",
+  storageBucket: "get-account-18-43109.firebasestorage.app",
+  messagingSenderId: "776922570919",
+  appId: "1:776922570919:web:58bf799181ed4d18b03b82",
+  measurementId: "G-MHSXL0FN75"
 };
-
 // Initialize Firebase for NSWF
 const nswfApp = initializeApp(firebaseConfig, "nswf");
 export const nswfDatabase = getDatabase(nswfApp);
